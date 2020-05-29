50222ec153189a1eca0b9a5d9d4af9be92ac274a
fabuloussTeam <tjomemmanuel@gmail.com>
2020-05-29 08:38:55 +0100

    Mise en place simple: preparation du projet

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:applicationdefluxrss/widgets/my_app.dart';
+
+void main() => runApp(MyApp());
+
+
+
---
+++ lib/widgets/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
