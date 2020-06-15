6fe0f413d784c7d29f2fe0ea45cbcf9bb4ba6515
fabuloussTeam <tjomemmanuel@gmail.com>
2020-06-15 11:33:19 +0100

    Correction du titre de l'application

--- lib/widgets/my_app.dart
+++ lib/widgets/my_app.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(title: 'Fab Flux News'),
+      home: Home(title: 'Fab News World Time'),
     );
   }
 }
